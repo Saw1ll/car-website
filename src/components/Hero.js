091a6dc566bd00eb5d384d321ae6c387c2296b09
Steven <williamssaw05@gmail.com>
2023-09-08 18:47:49 +0100

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,30 @@
+// hero is a large image spanning across the webpage
+import React from "react";
+import { Button } from './Button';
+import '../App.css';
+import './Hero.css';
+import videos from '../videos/nurburgringDrive.mp4';
+
+function Hero() {
+    return (
+    <div className="hero-container">
+        <video src={videos} autoPlay loop muted />
+        <h1>THIS COULD BE YOU!</h1>
+        <p>What are you waiting for?</p>
+        <div className="hero-btns">
+            <Button className="btns"
+                buttonStyle="btn--outline"
+                buttonSize='btn--large'
+            >BEGIN YOUR JOURNEY!</Button>
+
+            <Button
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize='btn--large'
+            >WATCH TRAILER!<div className="play-circle" /></Button>
+        </div>
+    </div>
+    )
+}
+
+export default Hero;
