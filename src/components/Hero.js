af84a60ae14a4e3ed4dfef42d774b14b44b5f2fa
Steven <williamssaw05@gmail.com>
2023-09-09 18:28:51 +0100

    updated css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,23 +7,23 @@ import videos from '../videos/nurburgringDrive.mp4';
 
 function Hero() {
     return (
-    <div className="hero-container">
-        <video src={videos} autoPlay loop muted />
-        <h1>THIS COULD BE YOU!</h1>
-        <p>What are you waiting for?</p>
-        <div className="hero-btns">
-            <Button className="btns"
-                buttonStyle="btn--outline"
-                buttonSize='btn--large'
-            >BEGIN YOUR JOURNEY!</Button>
+        <div className="hero-container">
+            <video src={videos} autoPlay loop muted />
+            <h1>THIS COULD BE YOU!</h1>
+            <p>What are you waiting for?</p>
+            <div className="hero-btns">
+                <Button className="btns"
+                    buttonStyle="btn--outline"
+                    buttonSize='btn--large'
+                >BEGIN YOUR JOURNEY!</Button>
 
-            <Button
-                className="btns"
-                buttonStyle="btn--primary"
-                buttonSize='btn--large'
-            >WATCH TRAILER!<div className="play-circle" /></Button>
+                <Button
+                    className="btns"
+                    buttonStyle="btn--primary"
+                    buttonSize='btn--large'
+                >WATCH TRAILER!<div className="play-circle" /></Button>
+            </div>
         </div>
-    </div>
     )
 }
 
